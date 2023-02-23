fa0e4122974dac1098af02bcb8b2cd97b6117b66
Oriol Gómez <oriol.gomez@wiris.com>
2023-02-23 14:56:42 +0100

    test: add basic quizzes test

--- test/specs/click.spec.js
+++ test/specs/click.spec.js
@@ -1,16 +1,18 @@
-describe('Find an element', () => {
-    it('Logins correctly', async () => {
-        // act
-        await browser.url('https://www.saucedemo.com/');
+describe('Wiris Quizzes demo page', () => {
+    it('Visits the Wiris Quizes demo page', async () => {
+        await browser.navigateTo('https://demo.wiris.com/quizzes/en/level3.html')
 
-        // Arrange
-        await (await $('#user-name')).setValue('standard_user');
-        await (await $('#password')).setValue('secret_sauce');
-        await (await $('#login-button')).click();
+await browser.$('#tabauthoring').click();
 
-        // Assert
-        await expect(browser).toHaveUrl('https://www.saucedemo.com/inventory.html');
-    })
-})
+await browser.$('#wrsUI_openStudio').click();
 
+await browser.$('#wrsUI_validationOptionsButton').click();
 
+await browser.$('#wrsUI_validationOptionsCustomGradingFunction').click();
+
+await browser.$('#wrsUI_validationOptionsCustomGradingFunctionFunctionNameTextField > input[type=text]').setValue('gf')
+
+await browser.$('#wrsUI_quizzesStudio > div.wrsUI_component.wrsUI_panel.wrsUI_activityContainer.wrsUI_borderPanelAtCenter > div.wrsUI_component.wrsUI_panel.wrsUI_flowPanel.wrsUI_flowPanelTopToBottom.wrsUI_activityPanel.wrsUI_panelSolid.wrsUI_quizzesStudioValidationOptions > div > div:nth-child(2) > div > div:nth-child(5) > div:nth-child(2) > div.wrsUI_component.wrsUI_panel.wrsUI_borderPanel > div.wrsUI_component.wrsUI_panel.wrsUI_flowPanel.wrsUI_flowPanelLeftToRight.wrsUI_borderPanelAtEast > button').click();
+await browser.pause(30000);
+    });
+});
