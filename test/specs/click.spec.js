5143d7e10939102c9112f8d7827b21ff01eb7342
atoro-at-wiris <alejandro.toro@wiris.com>
2023-03-09 14:40:34 +0100

    test: advancing on feature

--- test/specs/click.spec.js
+++ test/specs/click.spec.js
@@ -5,9 +5,15 @@ describe('Wiris Quizzes demo page', () => {
         await browser.$('#wrsUI_openStudio').waitForDisplayed({timeout: 5000})
         await browser.$('#wrsUI_openStudio').click();
         await browser.$('#wrsUI_addNewSimpleRandomVariableButton').click();
-        await browser.pause(5000);
+        await browser.$('#wrsUI_basicRandomVariableName0').waitForDisplayed({timeout: 5000});
         await browser.$('#wrsUI_basicRandomVariableName0').click();
-        await browser.keys('ei');
+        await browser.keys('sol');
+        await browser.$('#wrsUI_basicRandomVariableMin0').click();
+        await browser.keys('1');
+        await browser.$('#wrsUI_basicRandomVariableMax0').click();
+        await browser.keys('10');
+        //await browser.pause(6000)
+        //await browser.$('#wrsUI_mainMathField > div > div > div.wrs_formulaDisplayWrapper > div.wrs_formulaDisplay > span.wrs_container').click();
         await browser.pause(10000);
     });
 });
