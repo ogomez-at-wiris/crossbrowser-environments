70a2fd2e91770964281cf492f4ed85021040fbc9
Oriol Gómez <oriol.gomez@wiris.com>
2023-03-21 16:04:01 +0100

    fix: maximize

--- test/specs/click.spec.js
+++ test/specs/click.spec.js
@@ -1,5 +1,6 @@
 describe('Wiris Quizzes demo page', () => {
     it('Visits the Wiris Quizes demo page', async () => {
+        await browser.maximizeWindow();
         await browser.navigateTo('https://demo.wiris.com/quizzes/en/level3.html')
         await browser.$('#tabauthoring').click();
         await (await browser.$('#wrsUI_openStudio').waitForDisplayed({ timeout: 5000 }))
@@ -24,7 +25,8 @@ describe('Wiris Quizzes demo page', () => {
         await browser.pause(6000);
         await browser.$('#wrsUI_variableOptionsButton').click();
         await browser.pause(4000);
-        const calcmeTextArea = await browser.$('#wrsUI_variableOptionsCalcMe > div > div.wrsUI_component.wrsUI_panel.wrsUI_flowPanel.wrsUI_flowPanelTopToBottom.wrsUI_sheetBox.wrsUI_borderPanelAtWest > div > div.wrsUI_component.wrsUI_panel.wrsUI_flowPanel.wrsUI_flowPanelLeftToRight.wrsUI_sheetItems > div.wrsUI_component.wrsUI_panel.wrsUI_flowPanel.wrsUI_flowPanelLeftToRight.wrsUI_focused > div.wrsUI_calcSheet > div > div.wrsUI_lineBox.wrsUI_algorithmBox.wrsUI_first.wrsUI_currentChild > div.wrsUI_container.wrsUI_lineMain.wrsUI_currentChild > div > div > div > div.CodeMirror-scroll > div.CodeMirror-sizer > div > div > div > div.CodeMirror-code');
+        await browser.debug();
+        const calcmeTextArea = await browser.$('.wrsUI_focused');
         await calcmeTextArea.waitForDisplayed();
         await calcmeTextArea.click();
         await browser.debug();
